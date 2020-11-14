1edc0f8eab7819261dab3ff23a4486cbc29579de
Stefan <stefan@ladderlife.com>
2020-11-14 12:34:38 -0600

    oops

--- src/common.py
+++ src/common.py
@@ -2,7 +2,7 @@ import platform
 import os
 import yaml
 
-debugging = "macOS" in platform.platform() or ("WEASLEY_DEBUG" in os.environ and os.environ["WEASLEY_DEBUG"] = "DEBUG")
+debugging = "macOS" in platform.platform() or ("WEASLEY_DEBUG" in os.environ and os.environ["WEASLEY_DEBUG"] == "DEBUG")
 interval = 60 if debugging else 240
 
 with open("clock_config.yaml", 'r') as stream:
