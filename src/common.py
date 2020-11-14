15b4acf09a1200001aa6215f6499619ada1e6068
Stefan <stefan@ladderlife.com>
2020-11-14 12:33:36 -0600

    another way of debugging

--- src/common.py
+++ src/common.py
@@ -1,7 +1,8 @@
 import platform
+import os
 import yaml
 
-debugging = "macOS" in platform.platform()
+debugging = "macOS" in platform.platform() or ("WEASLEY_DEBUG" in os.environ and os.environ["WEASLEY_DEBUG"] = "DEBUG")
 interval = 60 if debugging else 240
 
 with open("clock_config.yaml", 'r') as stream:
